a810d5b365abfdd2eb7d398b8e275e1fc219f122
Kariaki58 <kariakistephen809@gmail.com>
2025-03-14 12:42:54 +0100

    more seo touch

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,11 +1,18 @@
 import Header from "@/components/app-ui/header";
 import ProductList from "@/components/app-ui/ProductList";
 import ProductDisplayCategory from "@/components/app-ui/Category";
+import Head from "next/head";
 
 export default async function Home({ searchParams }) {
   const params = await searchParams;
   return (
     <main>
+      <Head>
+        <meta property="og:title" content="Shop the Best Boutique Fashion | poshwears" />
+        <meta property="og:description" content="Discover stylish boutique fashion, latest trends, and exclusive collections." />
+        <meta property="og:image" content="https://res.cloudinary.com/dviwggb7g/image/upload/v1732468902/images/IMG-20241124-WA0002_qmkt7a.jpg" />
+        <meta property="og:url" content="https://poshwears.ng" />
+      </Head>
       <Header />
       <ProductDisplayCategory searchParams={params}/>
       <ProductList searchParams={params} />
